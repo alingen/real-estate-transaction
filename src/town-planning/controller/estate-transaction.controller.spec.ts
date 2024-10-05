f5ee23921fb025bf020620a27fcd84c3cb618d2b
ike <kot.90313@gmail.com>
2024-10-05 20:31:04 +0900

    feat: create test case

---
+++ src/town-planning/controller/estate-transaction.controller.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EstateTransactionController } from './estate-transaction.controller';
+import { GetRealEstateTransactionUseCase } from '../usercase/get-real-estate-transaction.usecase';
+
+describe('EstateTransactionController', () => {
+  let controller: EstateTransactionController;
+  let useCase: GetRealEstateTransactionUseCase;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EstateTransactionController],
+      providers: [
+        {
+          provide: GetRealEstateTransactionUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<EstateTransactionController>(EstateTransactionController);
+    useCase = module.get<GetRealEstateTransactionUseCase>(GetRealEstateTransactionUseCase);
+  });
+
+  // controller.getRealEstateTransaction メソッドが正しく呼び出される
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  // 正しいクエリパラメーターで useCase.execute メソッドが呼び出される
+  it('should call useCase.execute with the correct query parameters', async () => {
+    const mockResponse = { result: 'some data' };
+    jest.spyOn(useCase, 'execute').mockResolvedValue(mockResponse);
+
+    const query = { year: 2020, prefCode: 13, cityCode: '13101', displayType: 1 };
+    const result = await controller.getRealEstateTransaction(query);
+
+    expect(result).toEqual(mockResponse);
+    expect(useCase.execute).toHaveBeenCalledWith(query);
+  });
+});
